d6fdd9febae729cee4950a85a464733b85fc0f97
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2021-03-02 11:12:00 +0100

    all members work

--- src/qr-code/qr-code.service.ts
+++ src/qr-code/qr-code.service.ts
@@ -11,10 +11,10 @@ export class QrCodeService {
   // has account / don't have account / expired : test scenarios
   // partner disabled / enabled / wrong code
   async create(createQrCodeDto: CreateQrCodeDto): Promise<QrCode> {
-    const partner = await this.prisma.partner.findOne({
+    const partner = await this.prisma.partner.findFirst({
       where: { code: createQrCodeDto.code },
     });
-    const member = await this.prisma.member.findOne({
+    const member = await this.prisma.member.findFirst({
       where: { id: createQrCodeDto.id },
       include: { ieeeAccount: true },
     });
@@ -36,14 +36,17 @@ export class QrCodeService {
 
     const res = {
       ...(await qr),
-      hasIEEEAcount: !!member.ieeeAccount,
-      expiredAccount: !!member.ieeeAccount && accountExpired(member),
+      hasIEEEAcount: true,
+      // hasIEEEAcount: !!member.ieeeAccount,
+      expiredAccount: false,
+      // expiredAccount: !!member.ieeeAccount && accountExpired(member),
     };
 
     return res;
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function accountExpired(memeber: Member & { ieeeAccount: IEEEAccount }) {
   const Difference_In_Time =
     new Date().getTime() - memeber.ieeeAccount.expirationDate.getTime();
